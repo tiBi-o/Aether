5aefa7dae4a2bec1add93942351a89f657cb0af8
tiBi-o <centibi.O@gmail.com>
2025-08-03 19:39:51 +0000

    Remove deleted folders

--- server/vitest.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-  test: {
-    include: ["__tests__/**/*.test.js"],
-    globals: true,
-    environment: "node",
-  },
-});
